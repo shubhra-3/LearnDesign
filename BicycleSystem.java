76fadbd8c3c1fe77eab0b8de552f31af70308fe1
Shubhra Choudhary <shubhra.choudhary123@gmail.com>
2020-07-12 21:03:59 +0530

    updates the class to add a feature to calculate gear inches, but as we see, the problem was apparent, everywhere we need to change (call to pararmeters)

--- BicycleSystem.java
+++ BicycleSystem.java
@@ -5,18 +5,24 @@ class Gear{
 	
 	int chainring; //number of teeth
 	int cog; 
-	double ratio; 
+	double ratio, rim, tire; 
 
-	Gear(int chainring, int cog){
+	Gear(int chainring, int cog, double rim, double tire){
 		this.chainring = chainring;
 		this.cog = cog;
+		this.rim = rim;
+		this.tire = tire;
 	}
 
-	double getRatio(){
+	double getRatio() {
 		ratio = (double)chainring/cog;
 		return ratio;
 	}
 
+	double getgearInches() {
+		double temp = ratio * (rim + (tire * 2));
+		return temp;
+	}
 
 }
 
@@ -24,7 +30,9 @@ public class BicycleSystem{
 	
 	public static void main( String[] args ){
 		
-		Gear g1 = new Gear(20, 11);
+		Gear g1 = new Gear(20, 11, 26, 1.5);
 		System.out.println("The ratio is " + g1.getRatio());
+		System.out.println("The gear inches is " + g1.getgearInches());
 	}
 }
+
